b51ff7bb40838bfc3aaf1d1f052471a7ff23ca84
railken <railken@protonmail.ch>
2019-04-06 22:43:22 +0200

    add lang

---
+++ resources/lang/en/stock.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name' => 'Stock',
+    'description' => 'Stock',
+    'attributes' => []
+]
---
+++ resources/lang/en/warehouse.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name' => 'Warehouse',
+    'description' => 'Warehouse',
+    'attributes' => []
+]
